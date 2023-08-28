68665604f8f2de69cdf31b4c62dd4af5703a5b50
Shiva Kanchi <shiva@bombaydc.com>
2023-08-28 14:29:30 +0530

    some update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,12 +9,9 @@
     {{-- <link href="styles/index.css" rel="stylesheet" /> --}}
     <link href="{{ asset('css/index.css') }}" rel="stylesheet" />
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
     <!-- Font -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Datatable -->
     <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css" rel="stylesheet" />
@@ -38,8 +35,7 @@
                 <div class="modal-body show_delete_task_content">
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal"
-                        onclick="deleteTask.apply(this, arguments)">Yes</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="deleteTask.apply(this, arguments)">Yes</button>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
                 </div>
             </div>
@@ -58,37 +54,35 @@
 
                         <input type="hidden" name="is_manager" id="is_manager" value="{{$active_user->is_manager}}">
                         <input type="hidden" name="is_selectedAll" id="is_selectedAll" value="{{request()->employee}}">
-                        
-                        <input type="hidden" id="manager_url" name="manager_url"
-                        value="{{ url('tasks/' . session('active_user_slug') . '/') }}">
+
+                        <input type="hidden" id="manager_url" name="manager_url" value="{{ url('tasks/' . session('active_user_slug') . '/') }}">
                         <input type="hidden" id="app_url" name="app_url" value="{{ url('tasks/') }}">
 
                         <div class="mb-3">
                             <label for="task_project" class="form-label">Project : </label>
 
-                            <select id="task_project" name="project_id" class="form-select" required
-                                aria-label="Default select example">
+                            <select id="task_project" name="project_id" class="form-select" required aria-label="Default select example">
                                 <option value="" disabled selected>Project Name</option>
                                 @foreach ($projects as $project)
-                                    <option value="{{ $project->id }}">{{ $project->name }}</option>
+                                <option value="{{ $project->id }}">{{ $project->name }}</option>
                                 @endforeach
                             </select>
                         </div>
 
                         <div class="mb-3">
                             <label for="task_title" class="form-label">Task : </label>
-                            <input type="text" class="form-control" id="task_title" placeholder="Task name"
-                                name="name" required />
+                            <!-- <input type="text" class="form-control" id="task_title" placeholder="Task name"
+                                name="name" required /> -->
+                            <textarea type="text" class="form-control" id="task_title" placeholder="Task name" name="name" required style="min-height: 80px"></textarea>
                         </div>
 
                         <div class="mb-3">
                             <label for="task_assigned_to" class="form-label">Assign to :
                             </label>
-                            <select id="task_assigned_to" name="user_id" class="form-select" required
-                                aria-label="Default select example">
+                            <select id="task_assigned_to" name="user_id" class="form-select" required aria-label="Default select example">
                                 <option value="" disabled selected>Employee Name</option>
                                 @foreach ($users as $user)
-                                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                <option value="{{ $user->id }}">{{ $user->name }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -96,8 +90,7 @@
 
                         <div class="mb-3">
                             <label for="priority" class="form-label">Priority : </label>
-                            <select id="priority" name="priority" class="form-select"
-                                aria-label="Default select example" required>
+                            <select id="priority" name="priority" class="form-select" aria-label="Default select example" required>
                                 <option value="1">High</option>
                                 <option value="0" selected>None</option>
                             </select>
@@ -110,8 +103,7 @@
                                 <label class="form-check-label" for="1"> Completed </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="status" id="0"
-                                    checked />
+                                <input class="form-check-input" type="radio" name="status" id="0" checked />
                                 <label class="form-check-label" for="0">
                                     Not Completed
                                 </label>
@@ -122,12 +114,10 @@
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
                             Close
                         </button>
-                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" onclick="saveTask()"
-                            id="updatetask">
+                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" onclick="saveTask()" id="updatetask">
                             Update
                         </button>
-                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal"
-                            onclick="handleSubmit()" id="savetask">
+                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" onclick="handleSubmit()" id="savetask">
                             Save
                         </button>
                     </form>
@@ -138,8 +128,7 @@
     </div>
 
     <!--show task modal-->
-    <div class="modal fade" id="showTaskModal" tabindex="-1" aria-labelledby="showTaskModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="showTaskModal" tabindex="-1" aria-labelledby="showTaskModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -199,50 +188,47 @@
         </section>
 
         <!--Tasks Section-->
-        <section class="" >
+        <section class="">
 
             <div class="border-top mt-5">
                 <div class="" id="tasksheading">
                     <h2 class="d-block" id="tasksdesc">
                         {{-- @if (!request()->employee) --}}
                         @if (!request()->employee && $active_user->is_manager)
-                            {{ $active_user->name . "'s team's tasks" }}
+                        {{ $active_user->name . "'s team's tasks" }}
                         @elseif (!count($users) && !$active_user->is_manager)
-                            {{ $active_user->name . "'s tasks" }}
+                        {{ $active_user->name . "'s tasks" }}
                         @else
-                            @foreach ($users as $user)
-                                {{ $user->id == request()->employee ? $user->name . "'s tasks" : '' }}
-                            @endforeach
+                        @foreach ($users as $user)
+                        {{ $user->id == request()->employee ? $user->name . "'s tasks" : '' }}
+                        @endforeach
                         @endif
 
                     </h2>
 
                     <div class=" d-flex align-items-end g-5  " id="tasksbuttons">
                         <div class="d-inline-block px-3">
-                            <select class="form-select" aria-label="Default select example" id="selectedEmployee"
-                                @if (!$active_user->is_manager) style="display: none;" @endif>
+                            <select class="form-select" aria-label="Default select example" id="selectedEmployee" @if (!$active_user->is_manager) style="display: none;" @endif>
                                 <option value="0" selected>All</option>
                                 @foreach ($users as $user)
-                                    <option value="{{ $user->id }}"
-                                        {{ $user->id == request()->employee ? 'selected' : '' }}>
-                                        {{ $user->name }}
-                                    </option>
+                                <option value="{{ $user->id }}" {{ $user->id == request()->employee ? 'selected' : '' }}>
+                                    {{ $user->name }}
+                                </option>
                                 @endforeach
                             </select>
                         </div>
                         @if ($active_user->is_manager)
-                            <button class="btn btn-primary d-inline" type="submit" data-bs-toggle="modal"
-                                data-bs-target="#addNewModal" onclick="reRenderData() ">
-                                <i class="fa-solid fa-plus"></i>
-                                Add new Task
-                            </button>
+                        <button class="btn btn-primary d-inline" type="submit" data-bs-toggle="modal" data-bs-target="#addNewModal" onclick="reRenderData() ">
+                            <i class="fa-solid fa-plus"></i>
+                            Add new Task
+                        </button>
                         @endif
                     </div>
                 </div>
             </div>
             <!--Task Contents-->
             <div class="mt-5 mb-5" id="tasks">
-                <table id="tasktable" class="cell-border" style="width:100%" >
+                <table id="tasktable" class="cell-border" style="width:100%">
                     <thead>
                         <tr>
                             <th>Assigned On</th>
@@ -250,12 +236,12 @@
                             <th>Project</th>
                             <th>Task</th>
                             @if ($active_user->is_manager && !request()->employee)
-                                <th>Assigned To</th>
+                            <th>Assigned To</th>
                             @endif
                             <th>Status</th>
 
                             @if ($active_user->is_manager)
-                                <th>Actions</th>
+                            <th>Actions</th>
                             @endif
                         </tr>
                     </thead>
@@ -285,61 +271,59 @@
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <!-- js -->
     <!-- sc -->
-        <script>
-        var tasklist = @json($tasks);
-        //   @if ($active_user->is_manager)
-        //     console.log('manager');
-        // @else
-        //     console.log('not manager');
-        // @endif
-        </script> 
-       <script src="{{ asset('js/index.js') }}"></script>
     <script>
-        // var tasklist = [{
-        //         id: "123",
-        //         project: "Asign",
-        //         task: "Pdf update",
-        //         assignedOn: "1692184282172",
-        //         priority: 1,
-        //         status: true,
-        //         assignedTo: "Sagar",
-        //     },
+    var tasklist = @json($tasks);
+    //   @if ($active_user->is_manager)
+    //     console.log('manager');
+    // @else
+    //     console.log('not manager');
+    // @endif
+    </script>
+    <script src="{{ asset('js/index.js') }}"></script>
+    <script>
+    // var tasklist = [{
+    //         id: "123",
+    //         project: "Asign",
+    //         task: "Pdf update",
+    //         assignedOn: "1692184282172",
+    //         priority: 1,
+    //         status: true,
+    //         assignedTo: "Sagar",
+    //     },
 
-        //     {
-        //         id: "456",
-        //         project: "Camel",
-        //         task: "Emailers",
-        //         assignedOn: "1692184282172",
-        //         priority: 0,
-        //         status: false,
-        //         assignedTo: "Sunil",
-        //     },
-        //     {
-        //         id: "789",
-        //         project: "TaskManager",
-        //         task: "Frontend update",
-        //         assignedOn: "1692184282172",
-        //         priority: 0,
-        //         status: false,
-        //         assignedTo: "Shiva",
-        //     },
-        // ];
-        // console.log(tasklist);
+    //     {
+    //         id: "456",
+    //         project: "Camel",
+    //         task: "Emailers",
+    //         assignedOn: "1692184282172",
+    //         priority: 0,
+    //         status: false,
+    //         assignedTo: "Sunil",
+    //     },
+    //     {
+    //         id: "789",
+    //         project: "TaskManager",
+    //         task: "Frontend update",
+    //         assignedOn: "1692184282172",
+    //         priority: 0,
+    //         status: false,
+    //         assignedTo: "Shiva",
+    //     },
+    // ];
+    // console.log(tasklist);
 
 
-      
 
 
-        // @if (!request()->employee)
-        //     console.log('all');
-        // @else
-        //     console.log('not all');
-        // @endif
-  
+
+    // @if (!request()->employee)
+    //     console.log('all');
+    // @else
+    //     console.log('not all');
+    // @endif
     </script>
     <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
     </script>
     <!-- Datatable -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
